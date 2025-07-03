b8398a9de4225c35bd78a63c599be7ab4127dc20
Jean-Leon <zjeanleon@yahoo.fr>
2025-07-03 11:19:28 +0200

    fix resolution du problème PORT

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import pandas as pd
-import os
-import uvicorn
+
 # ✅ Charger le modèle binaire (2 classes : -1, +1)
 with open("gold_cross_prediction_model.pkl", "rb") as f:
     model = pickle.load(f)
@@ -61,10 +60,3 @@ def predict(features: InputFeatures):
             "+1 (HAUSSE)": round(p_hausse * 100, 2)
         }
     }
-if __name__ == "__main__":
-    uvicorn.run(
-        "app:app",
-        host="0.0.0.0",
-        port=int(os.environ.get("PORT", 8080)),
-        reload=False
-    )
